f25a3e43a779dc4d9bdf5cfcb4e03dfe9a8cb307
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-02 18:35:36 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,12 @@ async fn signin()-> axum::response::Response<String> {
 struct Signin {
     ac: String,
 }
-async fn ddbb(a:String) {
+async fn ddbb(a:String) -> Result{
 	let client = Client::with_options(ClientOptions::parse("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await?)?;
 	let db = client.database("braq").collection("users");
 	let aac = db.insert_one(doc!{"un":a},None).await?;
 }
-async fn signin_form()-> axum::response::Response<String> {
+async fn signin_form(Form(signin): Form<Signin>)-> axum::response::Response<String> {
 	let ac = signin.ac;
 	ddbb(ac);
 	let mut context = Context::new();
