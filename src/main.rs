fac20a9f30e95277fde6fd917d69c45f29acbc64
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-03 10:05:01 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -34,11 +34,13 @@ async fn signin()-> axum::response::Response<String> {
         .header("Content-Type", "text/html; charset=utf-8")
         .body(tera.render("signin", &Context::new()).unwrap()).unwrap()
 }
-
-async fn signin_form()-> axum::response::Response<String> {
+pub struct CreateUser {
+    pub ac: String,
+}
+async fn signin_form(Form(CreateUser): Form<CreateUser>,)-> axum::response::Response<String> {
 	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.expect("Failed to connect");
 	let db = client.database("braq").collection("users");
-	db.insert_one(doc!{"un":"gg"},None).await.expect("Failed to connect2");
+	db.insert_one(doc!{"un":payload.ac},None).await;
 	let mut context = Context::new();
 	let mut tera = Tera::default();
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
