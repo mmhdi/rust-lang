7ab628bb8e89d307a5880d72c3bca2bdbc59ed32
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-03 13:14:49 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -34,8 +34,6 @@ async fn signin()-> axum::response::Response<String> {
         .header("Content-Type", "text/html; charset=utf-8")
         .body(tera.render("signin", &Context::new()).unwrap()).unwrap()
 }
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
 async struct CreateUser {
     ac: String,
 }
