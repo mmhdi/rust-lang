e7d3bd77b76e725f7009a6d8e306bf6f11e26c36
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-03 16:22:49 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ async fn signin_form(Form(CreateUser): Form<CreateUser>)-> axum::response::Respo
 	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.expect("Failed to connect");
 	let db = client.database("braq").collection("users");
 	db.insert_one(doc!{"un":CreateUser.ac},None).await;
-	let mut context = Context::new();
+	let context = Context::new();
 	let mut tera = Tera::default();
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
 	Response::builder().status(axum::http::StatusCode::OK)
