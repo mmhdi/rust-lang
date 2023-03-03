4eac62ea31a81891a2e806b60081c4170afb961c
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-03 16:52:07 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ async fn signin()-> axum::response::Response<String> {
 struct CreateUser {
     ac: String,
 }
-#[debug_handler]
+#[debug_signin_form]
 async fn signin_form(Form(CreateUser): Form<CreateUser>){
 	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.expect("Failed to connect");
 	let db = client.database("braq").collection("users");
