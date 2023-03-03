b2b3886b7d8dcc1ea84d691ed900c7377223f0d5
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-03 16:19:05 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tokio;
-use axum::{extract::Form, routing::get, response::Response, Router};
+use axum::{extract::Form, routing::get, routing::post, response::Response, Router};
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
 use mongodb::{bson::doc, Client};
