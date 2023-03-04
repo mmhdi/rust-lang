f13deddf4a209ee37676ee83868a9042959adeae
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 01:02:36 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -40,15 +40,18 @@ struct Login {
     ac: String,
     pw: String
 }
-
+struct Loginn {
+    ac: String,
+    pw: String
+}
 async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
 	let client = Client::with_options(ClientOptions::parse("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap()).unwrap();
-	let db = client.database("braq").collection::<Login>("users");
+	let db = client.database("braq").collection::<Loginn>("users");
 	let deb: Login = db.find_one(doc!{"un":login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
-	context.insert("ac",&deb.pw.unwrap());
+	context.insert("ac",&deb.pw);
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
 	Response::builder().status(axum::http::StatusCode::OK)
         .header("Content-Type", "text/html; charset=utf-8")
