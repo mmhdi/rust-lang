3445d7178e2f2b6a21f9329934a2f185aefe5c6b
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 09:02:03 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,6 @@ struct Login {
     ac: String,
     pw: String
 }
-
 async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
 	let client = Client::with_options(ClientOptions::parse("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap()).unwrap();
 	let db = client.database("braq").collection::<Document>("users");
@@ -48,7 +47,7 @@ async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
-	context.insert("ac",&deb.get_str("pw").unwrap());
+	context.insert("ac",&deb.get_str("un").unwrap());
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
 	Response::builder().status(axum::http::StatusCode::OK)
         .header("Content-Type", "text/html; charset=utf-8")
