e8305b5f7b80c50598492c118dda402c942886e2
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 09:40:12 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,9 @@ async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
-	context.insert("ac",&deb.get_str("un"));
+	if login.ac == deb.get_str("un").unwrap() && login.pw == deb.get_str("pw").unwrap(){
+	context.insert("ac",&deb.get_str("un").unwrap());
+	}
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
 	Response::builder().status(axum::http::StatusCode::OK)
         .header("Content-Type", "text/html; charset=utf-8")
