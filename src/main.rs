9ea1ae99231d446406b667c97dd68220d7d24c04
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 18:56:18 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum::{extract::Form, routing::get, response::Response, Router};
 use serde::{Deserialize, Serialize};
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
-use mongodb::{bson::{doc,Document},options::ClientOptions,Client};
+use mongodb::{bson::{doc,Document},Client};
 
 #[tokio::main]
 async fn main() {
@@ -41,13 +41,13 @@ struct Login {
     pw: String
 }
 async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
-	let client = Client::with_options(ClientOptions::parse("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap()).unwrap();
+	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
 	let db = client.database("braq").collection::<Document>("users");
 	let deb: Document = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
-	if &deb.get_str("un").unwrap() == &login.ac && &deb.get_str("pw").unwrap() == &login.pw{
+	if deb && &deb.get_str("un").unwrap() == &login.ac && &deb.get_str("pw").unwrap() == &login.pw{
 		context.insert("ac",&deb.get_str("un").unwrap());
 	}else{
 		context.insert("ac","none");
