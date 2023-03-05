b5b6a5ead0921e0e6e57cdd011ce889ef1feafbd
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 23:22:20 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -40,15 +40,23 @@ struct Login {
 	ac: String,
     pw: String
 }
+#[derive(Deserialize, Serialize)]
+struct Loginn {
+	un: String,
+    em: String,
+    ln: String,
+    ph: String,
+    pw: String
+}
 async fn handler(Form(login): Form<Login>)-> impl IntoResponse{
 	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
-	let db = client.database("braq").collection::<Document>("users");
-	let deb: Document = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
+	let db = client.database("braq").collection::<Loginn>("users");
+	let deb: Loginn = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
 	//if &deb.get_str("un") == &login.un && &deb.get_str("pw") == &login.pw{
-		context.insert("ac",&deb.get_str("un"));
+		context.insert("ac",&deb.un);
 	//}else{
 		//context.insert("ac","none");
 	//}
