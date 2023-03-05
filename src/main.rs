f2bab1411f6e07e0f18682d6886b90416b9abdba
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 13:35:02 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -43,15 +43,11 @@ struct Login {
 async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
 	let client = Client::with_options(ClientOptions::parse("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap()).unwrap();
 	let db = client.database("braq").collection::<Document>("users");
-	let deb: Document = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
+	let deb: Document = db.find_one(doc!{"un":login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
-	if &deb.get_str("un").unwrap() == &login.ac && &deb.get_str("pw").unwrap() == &login.pw{
-		context.insert("ac",&deb.get_str("un").unwrap());
-	}else{
-		context.insert("ac","none");
-	}
+	
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
 	Response::builder().status(axum::http::StatusCode::OK)
         .header("Content-Type", "text/html; charset=utf-8")
