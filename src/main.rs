4a58e607ef3bd86875316962c78a72c7ae6fc3bd
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-05 17:01:42 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum::{extract::Form, routing::get, response::Response, Router};
 use serde::{Deserialize, Serialize};
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
-use mongodb::{bson::{doc,Document},options::ClientOptions, Client};
+use mongodb::{bson::{doc,Document},Client};
 
 #[tokio::main]
 async fn main() {
@@ -41,7 +41,7 @@ struct Login {
     pw: String
 }
 async fn handler(Form(login): Form<Login>)-> axum::response::Response<String>{
-	let client = Client::with_options(ClientOptions::parse("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap()).unwrap();
+	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
 	let db = client.database("braq").collection::<Document>("users");
 	let deb: Document = db.find_one(doc!{"un":login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
