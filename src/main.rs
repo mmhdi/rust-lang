fa45ee3ba30ac597cd9bb865cf7b61c64bffbdb8
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-06 16:49:41 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ struct Login {
     ac: Option<String>
 }
 async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, StatusCode> {
-	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.ok()?;
+	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.map_err(INTERNAL_SERVER_ERROR)?;
 	let db = client.database("braq").collection::<Login>("users");
 	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
