18427cbcc63f75b40c089216dbc9267a09ffa838
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-06 20:51:13 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ struct Login {
 }
 async fn signin_form(Form(login): Form<Login>)-> impl IntoResponse{
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Login>("users");
-	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await?;
+	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await??;
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
 	let mut tera = Tera::default();
 	let mut context = Context::new();
