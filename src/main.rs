3b791d197e8fb2d5456266acc425bd26a8e05cd7
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-06 13:22:57 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
 	axum::Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(Router::new()
 		.route("/", get(index))
 		.fallback_service(ServeDir::new("static"))
-		.route("/signin/", get(signin).post(signin))
+		.route("/signin/", get(signin).post(signin_form))
 		.route("/signup/", get(signup).post(signup_form))
 		.route("/confirm/email/", get(confirm_email).post(confirm_email_form))
 		.route("/confirm/email/verify/", get(confirm_email_verify).post(confirm_email_verify_form))
@@ -47,7 +47,7 @@ struct Login {
     otpemurl: Option<String>,
     ac: Option<String>
 }
-async fn signin(Form(login): Form<Login>)-> axum::response::Result<&'static str> {
+async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse,StatusCode> {
 	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.?;
 	let db = client.database("braq").collection::<Login>("users");
 	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
