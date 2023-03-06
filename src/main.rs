29382a1f2f1cadb6246cd49b4c93466e30fcc653
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-06 10:11:12 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -51,20 +51,18 @@ async fn handler(Form(login): Form<Login>)-> Result<Response<String>,()>{
 	let client = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.?;
 	let db = client.database("braq").collection::<Login>("users");
 	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
-	Ok(
-		//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
-		let mut tera = Tera::default();
-		let mut context = Context::new();
-		//if &deb.get_str("un") == &login.un && &deb.get_str("pw") == &login.pw{
-			context.insert("ac",&deb.em);
-		//}else{
-			//context.insert("ac","none");
-		//}
-		tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
-		Response::builder().status(axum::http::StatusCode::OK)
-	        .header("Content-Type", "text/html; charset=utf-8")
-	        .body(tera.render("signin", &context).unwrap()).unwrap()
-	)
+	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
+	let mut tera = Tera::default();
+	let mut context = Context::new();
+	//if &deb.get_str("un") == &login.un && &deb.get_str("pw") == &login.pw{
+		Ok(context.insert("ac",&deb.em));
+	//}else{
+		Err(context.insert("ac","none"));
+	//}
+	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
+	Response::builder().status(axum::http::StatusCode::OK)
+        .header("Content-Type", "text/html; charset=utf-8")
+        .body(tera.render("signin", &context).unwrap()).unwrap()
 }
 
 async fn signup()-> axum::response::Response<String> {
