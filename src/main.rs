b8793863b2cb18861f8edb5f16d601b71bef84ec
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-08 01:49:07 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ struct Login {
     otpemurl: Option<String>,
     ac: Option<String>
 }
-async fn signin_form(Form(login): Form<Login>)-> mongodb::error::Result<impl IntoResponse, impl IntoResponse> {
+async fn signin_form(Form(login): Form<Login>)-> mongodb::error::Result<impl IntoResponse> {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection("users");
 	//let deb: Login = db.find_one(doc!{"un":&login.ac},None).await;
 	db.insert_one(doc!{"un":login.ac},None).await?;
