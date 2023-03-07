5fb856a8392bf947e85f46619432cb6cfecd37d0
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-07 16:26:21 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ struct Login {
     otpemurl: Option<String>,
     ac: Option<String>
 }
-async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, impl IntoResponse>{
+async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, dyn Error>{
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await?.database("braq").collection::<Login>("users");
 	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await.unwrap().unwrap();
 	//let ggg= db.insert_one(doc!{"un":login.ac},None).await.unwrap();
@@ -62,9 +62,6 @@ async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, impl 
 	//}
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
 	Ok(Response::builder().status(axum::http::StatusCode::OK)
-        .header("Content-Type", "text/html; charset=utf-8")
-        .body(tera.render("signin", &context).unwrap()).unwrap());
-    Err(Response::builder().status(axum::http::StatusCode::OK)
         .header("Content-Type", "text/html; charset=utf-8")
         .body(tera.render("signin", &context).unwrap()).unwrap())
 }
