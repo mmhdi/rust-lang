61aa30f7adc99bf2969f7bae08003cbbe4fbff6c
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-08 11:47:55 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, &'sta
 	let mut context = Context::new();
 	//match deb{
 	//if &deb.get_str("un") == &login.un && &deb.get_str("pw") == &login.pw{
-		context.insert("ac",deb.em);
+		context.insert("ac",&deb.em);
 	//}else{
 		//Err => context.insert("ac","none")
 	//}
