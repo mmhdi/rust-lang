e9cb9fb65c6d86dd61c5e7f4e40e1d33cee56f00
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-08 22:50:25 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ struct Login {
 	otpemurl: Option<String>,
 	ac: Option<String>
 }
-async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, impl From<&'_ str> for Box<dyn Error>> {
+async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, impl Error<String>> {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Login>("users");
 	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await?;
 	//db.insert_one(doc!{"un":login.ac},None).await.map_err(|_| "read file error")?;
