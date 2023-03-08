f49a2faf9d2395cef18cd924b006138db73a3f89
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-08 12:06:43 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -51,14 +51,14 @@ struct Login {
     ac: Option<String>
 }
 async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse, &'static str> {
-	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Login>("users");
-	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await.map_err(|_| "read file error")?;
+	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection("users");
+	let deb = db.find_one(doc!{"un":&login.ac},None).await.map_err(|_| "read file error")?;
 	//db.insert_one(doc!{"un":login.ac},None).await.map_err(|_| "read file error")?;
 	let mut tera = Tera::default();
 	let mut context = Context::new();
 	//match deb{
 	//if &deb.get_str("un") == &login.un && &deb.get_str("pw") == &login.pw{
-		context.insert("ac",&deb.em);
+		context.insert("ac",&deb);
 	//}else{
 		//Err => context.insert("ac","none")
 	//}
