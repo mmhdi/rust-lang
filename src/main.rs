a9370cd3e7ecc743036ceecf89856501938e350a
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-09 17:02:14 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -52,9 +52,9 @@ struct Login {
 	otpemurl: Option<String>,
 	ac: Option<String>
 }
-async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse,String> {
+async fn signin_form(Form(login): Form<Login>)-> impl IntoResponse {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Login>("users");
-	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await?.unwrap();
+	let deb: Login = db.find_one(doc!{"un":&login.ac},None).await.unwrap()?;
 	//db.insert_one(doc!{"un":login.ac},None).await.map_err(|_| "read file error")?;
 	let mut tera = Tera::default();
 	let mut context = Context::new();
@@ -64,9 +64,9 @@ async fn signin_form(Form(login): Form<Login>)-> Result<impl IntoResponse,String
 		context.insert("ac","none")
 	}
 	tera.add_raw_templates(vec![("signin", include_str!("layouts/signin.html")),("header", include_str!("layouts/partials/header.html")),("footer", include_str!("layouts/partials/footer.html"))]).unwrap();
-	Ok(Response::builder().status(axum::http::StatusCode::OK)
+	Response::builder().status(axum::http::StatusCode::OK)
 		.header("Content-Type", "text/html; charset=utf-8")
-		.body(tera.render("signin", &context).unwrap()).unwrap())
+		.body(tera.render("signin", &context).unwrap()).unwrap()
 }
 
 
