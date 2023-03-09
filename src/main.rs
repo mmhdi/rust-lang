29dd94892175cf191cc0ea121984ae9b92faa25d
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-09 18:07:38 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ struct Login {
 }
 async fn signin_form(Form(login): Form<Login>)-> Result<Response<String>,String> {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Login>("users");
-	let deb: Option<Login> = Some(db.find_one(doc!{"un":&login.ac},None).await);
+	let deb: Option<Login> = Some(db.find_one(doc!{"un":&login.ac},None).await).unwrap()?;
 	//db.insert_one(doc!{"un":login.ac},None).await.map_err(|_| "read file error")?;
 	let mut tera = Tera::default();
 	let mut context = Context::new();
