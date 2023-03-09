3cdc82be81fbe89b1d9ca24078c09cae160be9c0
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-09 19:34:21 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -56,8 +56,8 @@ async fn signin_form(Form(sign): Form<Sign>)-> impl IntoResponse {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Sign>("users");
 	let mut context = Context::new();
 	match db.find_one(doc!{"un":&sign.ac},None).await) {
-		Ok(context.insert("ac","signed in")),
-		Err(context.insert("ac","signed not"))
+		Some(doc) => context.insert("ac","signed in {}",doc),
+		None => context.insert("ac","signed not")
 	};
 	//db.insert_one(doc!{"un":login.ac},None).await.map_err(|_| "read file error")?;
 	let mut tera = Tera::default();
