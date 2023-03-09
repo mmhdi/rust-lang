5dd027261af4cda2f0240a40780673996f6e0e7b
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-09 20:38:16 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use serde::{Deserialize, Serialize};
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
 use mongodb::{bson::doc,Client};
-use axum::http::StatusCode;
-use std::error::Error;
 
 #[tokio::main]
 async fn main() {
@@ -56,7 +54,7 @@ async fn signin_form(Form(sign): Form<Sign>)-> impl IntoResponse {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Sign>("users");
 	let mut context = Context::new();
 	match db.find_one(doc!{"un":&sign.ac},None).await.unwrap() {
-		Some(doc) => context.insert("ac","signed in {doc}"),
+		Some => context.insert("ac","signed in {doc}"),
 		None => context.insert("ac","signed not")
 	};
 	//db.insert_one(doc!{"un":login.ac},None).await.map_err(|_| "read file error")?;
