bb9a0b4a0c7974e14133ef7e593248e14adcd5b6
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-09 22:39:41 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ async fn signin()-> impl IntoResponse {
 
 
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, PartialEq)]
 struct Sign {
 	r#fn: Option<String>,
 	ln: Option<String>,
@@ -54,7 +54,7 @@ async fn signin_form(Form(sign): Form<Sign>)-> impl IntoResponse {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Sign>("users");
 	let deb: Sign = db.find_one(doc!{"un":&sign.ac},None).await.unwrap().unwrap();
 	let mut context = Context::new();
-	if (Sign{}) == sign {
+	if deb == nil {
 		context.insert("ac","none")
 	}else{
 		context.insert("ac",&deb.em);
