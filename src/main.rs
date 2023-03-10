dae3df82aacbb77c5c2d6f72cae3146a9886d1da
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-10 12:02:18 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ struct Signup {
 	ac: Option<String>
 }
 async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
-	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection::<Signup>("users");
+	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq").collection("users");
 	let mut context = Context::new();
 	if signup.r#fn.is_none(){
 		context.insert("fn","يجب كتابة الإسم الأول")
@@ -114,7 +114,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 		context.insert("rpw","يجب كتابة كلمة المرور مرتين بشكل متطابق")
 	}
 	
-	db.insert_one(doc!{"fn":signup.r#fn},None).await.unwrap();
+	db.insert_one(doc!{"fn":signup.r#fn,"ln":signup.ln,"un":signup.un,"em":signup.em,"pw":signup.pw},None).await.unwrap();
 	//match db.find_one(doc!{"un":&sign.ac},None).await.unwrap() {
 		//Some(u) => context.insert("ac","signed it"),
 		//None => context.insert("ac","signed not")
