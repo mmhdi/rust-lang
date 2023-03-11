6afd988b01b3af37e33af055e37f350c21952581
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-11 19:36:51 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ struct Signup {
 	ac: Option<String>
 }
 async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
-	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
+	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq");
 	let mut context = Context::new();
 	if signup.r#fn == Some("".to_string()){
 		context.insert("fn","يجب كتابة الإسم الأول")
@@ -102,7 +102,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 	if signup.un == Some("".to_string()){
 		context.insert("un","يجب كتابة إسم المستخدم")
 	}else{
-		let mut fun = match db.database("braq").collection::<Signup>("users").find_one(doc!{"un":&signup.un},None).await.unwrap() {
+		let mut fun = match db.collection::<Signup>("users").find_one(doc!{"un":&signup.un},None).await.unwrap() {
 			Some(..) =>{},
 			None =>{}
 		};
@@ -123,7 +123,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 		context.insert("rpw","يجب كتابة كلمة المرور مرتين بشكل متطابق")
 	}
 	if signup.r#fn != Some("".to_string()) && signup.ln != Some("".to_string()) && signup.un != Some("".to_string()) && signup.em != Some("".to_string()) && signup.pw != Some("".to_string()) && signup.rp != Some("".to_string()) && signup.pw == signup.rp {
-		db.database("braq").collection("users").insert_one(doc!{"fn":signup.r#fn,"ln":signup.ln,"un":signup.un,"em":signup.em,"pw":signup.pw,"status":"unen"},None).await.unwrap();
+		db.collection("users").insert_one(doc!{"fn":signup.r#fn,"ln":signup.ln,"un":signup.un,"em":signup.em,"pw":signup.pw,"status":"unen"},None).await.unwrap();
 	}
 	
 	
