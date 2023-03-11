957236aecc7558e3bce985966b7d1c04d0f169e1
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-11 10:15:26 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -106,7 +106,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 			Some(f) =>{},
 			None =>{}
 		};
-		if fun == Some{
+		if fun == Some(){
 			context.insert("un","يجب اختيار إسم المستخدم آخر")
 		}
 	}
