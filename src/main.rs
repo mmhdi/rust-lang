0883dd0a9ef978deead8a37bf1ed08aa458659d3
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-12 18:53:13 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
 use mongodb::{bson::doc,Client};
+use rand::rngs::StdRng;
 
 #[tokio::main]
 async fn main() {
@@ -94,7 +95,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq");
 	let mut context = Context::new();
 	if signup.r#fn == Some("".to_string()){
-		context.insert("fn","يجب كتابة الإسم الأول")
+		context.insert("fn",&StdRng::from_rng(thread_rng()).unwrap().rng.gen_range(0..100))
 	}
 	if signup.ln == Some("".to_string()){
 		context.insert("ln","يجب كتابة الإسم الأخير")
