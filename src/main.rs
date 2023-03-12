9cc957fea77e615a06e1201b695330cbe2d57a91
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-12 08:53:42 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -103,7 +103,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 		context.insert("un","يجب كتابة إسم المستخدم")
 	}else{
 		let fun = match db.collection::<Signup>("users").find_one(doc!{"un":&signup.un},None).await.unwrap() {
-			Some(okkk) =>(),
+			Some() =>(),
 			None =>()
 		};
 		if fun == Some(){
