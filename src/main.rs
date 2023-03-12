443181d4bbcf086f597e5e038f98b35a3313f732
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-12 21:07:50 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -95,7 +95,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 	let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq");
 	let mut context = Context::new();
 	if signup.r#fn == Some("".to_string()){
-		context.insert("fn",&StdRng::from_entropy().gen_range(100000..999999))
+		context.insert("fn",&StdRng::from_entropy().gen_range(1000000..9999999))
 	}
 	if signup.ln == Some("".to_string()){
 		context.insert("ln","يجب كتابة الإسم الأخير")
