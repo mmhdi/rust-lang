f90be77ee06236408e5b6ea0c14bdbfc6507dc8e
Mustafa Mahdi <107911372+mmhdi@users.noreply.github.com>
2023-03-12 16:50:44 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -104,8 +104,7 @@ async fn signup_form(Form(signup): Form<Signup>)-> impl IntoResponse {
 	}else{
 		let db = Client::with_uri_str("mongodb+srv://mbra:mbra@cluster0.um0c2p7.mongodb.net/?retryWrites=true&w=majority").await.unwrap().database("braq");
 		match db.collection::<Signup>("users").find_one(doc!{"un":&signup.un},None).await {
-			Ok(fun),
-			_
+			(Ok(fun),_)
 		}
 	}
 	if signup.em == Some("".to_string()){
